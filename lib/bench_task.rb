070254e267ac5416ab94b924235223de6f693158
Lenny Turetsky <lenny@pivotallabs.com>
2014-02-21 17:44:50 -0800

    handle tasks with no block (e.g., meta-tasks that just point to other tasks)

--- lib/bench_task.rb
+++ lib/bench_task.rb
@@ -2,10 +2,10 @@ require 'benchmark'
 
 module Rake
   module DSL
-    def bench_task(*args, &block)
-      task(*args) do |t, args|
+    def bench_task(*task_args, &block)
+      task(*task_args) do |t, args|
         puts Benchmark.measure {
-          block.call(t, args)
+          block.call(t, args) if block
         }
       end
     end
